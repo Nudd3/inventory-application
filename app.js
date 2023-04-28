55073dd8b640d14e2cf9959eb513a91fdc13f6fc
Noak Pettersson <noake12@gmail.com>
2023-04-29 00:42:50 +0200

    Created Category and Item models

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -6,6 +7,8 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
+
 
 const app = express();
 
