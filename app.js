d854def156db5a68b90074bd555bf456ea453517
Noak Pettersson <noake12@gmail.com>
2023-05-02 16:00:12 +0200

    optimized for railway

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
+
+const RateLimit = require('express-rate-limit');
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+app.use(limiter);
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
@@ -29,6 +38,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
